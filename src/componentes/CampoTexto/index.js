8b26e0aaf7506eac71e08ff7abffd5e4a3cd562d
silvioitallo <silvio_itallo@hotmail.com>
2023-02-07 20:50:18 -0300

    Primeira tela da Organo

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,23 @@
+import './CampoTexto.css';
+
+const CampoTexto = (props) => {
+    
+    const placeholderModificada = `${props.placeholder}...`;
+
+    //const [valor, setValor] = useState('')
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto;
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,18 @@
+import './ListaSuspensa.css';
+
+
+const ListaSuspensa = (props) => {
+    return (
+        <div className='Lista-suspensa'>
+            <label>{props.label}</label>
+            <select 
+                onChange={evento => props.aoAlterado(evento.target.value)} 
+                value={props.value} 
+                required={props.obrigatorio}>
+            {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    );
+}
+
+export default ListaSuspensa;
