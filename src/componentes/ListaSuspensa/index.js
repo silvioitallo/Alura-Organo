42882f3052386c39f042efffa55423c4fbc68456
silvioitallo <silvio_itallo@hotmail.com>
2023-02-15 17:56:39 -0300

    Corrigindo o card de colaboradores

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -7,9 +7,14 @@ const ListaSuspensa = (props) => {
             <label>{props.label}</label>
             <select 
                 onChange={evento => props.aoAlterado(evento.target.value)} 
-                value={props.value} 
+                value={props.valor} 
                 required={props.obrigatorio}>
-            {props.itens.map(item => <option key={item}>{item}</option>)}
+                <option value=""></option>
+
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                }
+                )}
             </select>
         </div>
     );
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -9,7 +9,7 @@ const Time = (props) => {
             <h3 style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
             
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)} 
+                {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)} 
             </div>
         </section>
     )
